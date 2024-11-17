114ee91cb4b71c0a4785a9b1e42bfc250c72063a
pavlov-kos <kostiy2424@gmail.com>
2024-11-17 14:52:55 +0300

    Add files via upload

---
+++ 'module_1_3.py'.py
@@ -0,0 +1,8 @@
+name='Konstantin' #str
+print(name, type(name))
+age=43 #int
+print(age, type(age))
+new_age=44 #int
+print(new_age, type(new_age))
+i_student=True #bool
+print(i_student, type(i_student))
